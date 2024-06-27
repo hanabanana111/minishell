6edc71a1503e3d474f1fc4905b1a16283f7dfab0
Hana Kobori <hakobori@c3r3s2.42tokyo.jp>
2024-06-28 05:20:29 +0900

    sgnal to signal

---
+++ srcs/signal.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 05:10:53 by hakobori          #+#    #+#             */
+/*   Updated: 2024/06/28 05:14:45 by hakobori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	signal_handler_sigint(int signum)
+{
+	(void)signum;
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	write(STDOUT_FILENO, "\n", 1);
+	rl_redisplay();
+}
+
+void	treat_signal(void)
+{
+	if (signal(SIGINT, signal_handler_sigint) == SIG_ERR)
+	{
+		perror(strerror(errno));
+		exit(1);
+	}
+	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+	{
+		perror(strerror(errno));
+		exit(1);
+	}
+}
