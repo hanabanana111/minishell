f9b044b3d2f03e4f9f9cfc442c9b926319945780
Ryo Kawaharada <rkawahar@c2r7s9.42tokyo.jp>
2024-06-27 21:14:51 +0900

    commitgit status

--- infile.c
+++ infile.c
@@ -6,11 +6,19 @@
 /*   By: rkawahar <rkawahar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 17:26:49 by rkawahar          #+#    #+#             */
-/*   Updated: 2024/06/27 20:44:40 by rkawahar         ###   ########.fr       */
+/*   Updated: 2024/06/27 21:11:34 by rkawahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	infile_redirect2(char *str)
+{
+	int	file_pipe[2];
+
+	if (pipe(file_pipe) < 0)
+		
+}
 
 int	determine_infile(t_info *lst)
 {
@@ -27,7 +35,7 @@ int	determine_infile(t_info *lst)
 		lst = lst -> next;
 	}
 	if (ft_strncmp(redirect, "<<\0", 3) == 0)
-		return (infile_redirect3(infile));
+		return (infile_redirect2(infile));
 	else
-		return (infile_redirect1());
+		return (infile_redirect1(infile));
 }
