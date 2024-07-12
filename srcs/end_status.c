c59e2967628a51f8fe3952b632bf6e1dccbd846d
Ryo Kawaharada <rkawahar@c3r6s3.42tokyo.jp>
2024-07-12 20:25:56 +0900

    endstatus

--- srcs/end_status.c
+++ srcs/end_status.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   end_status.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rkawahar <rkawahar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 16:41:07 by hakobori          #+#    #+#             */
-/*   Updated: 2024/07/09 21:25:12 by hakobori         ###   ########.fr       */
+/*   Updated: 2024/07/12 20:25:24 by rkawahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	end_status_func(int status)
 {
 	static int	end_status;
 
-	if (status > 0)
+	if (status >= 0)
 		end_status = status;
 	else
 		return (end_status);
