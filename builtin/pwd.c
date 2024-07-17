11059b630772e10089dd11f451702486de3b9c49
Hana Kobori <hakobori@c4r2s11.42tokyo.jp>
2024-07-17 15:23:02 +0900

    pwd func :)

--- builtin/pwd.c
+++ builtin/pwd.c
@@ -6,13 +6,19 @@
 /*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 14:53:24 by hakobori          #+#    #+#             */
-/*   Updated: 2024/07/17 14:56:18 by hakobori         ###   ########.fr       */
+/*   Updated: 2024/07/17 15:22:23 by hakobori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int pwd_func(char **env, )
+int pwd_func(char **env, t_cmd *lst)
 {
+	char *value;
 	
+	value = get_value(env,"PWD");
+	if(!value)
+		return(FALSE);
+	printf("%s\n",value);
+	return(TRUE);
 }
