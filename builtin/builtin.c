debd1f498da990337dcd2f54be50e05524cd3c5d
Hana Kobori <hakobori@c3r3s3.42tokyo.jp>
2024-07-18 00:09:09 +0900

    exit built in

--- builtin/builtin.c
+++ builtin/builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kawaharadaryou <kawaharadaryou@student.    +#+  +:+       +#+        */
+/*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:33:28 by hakobori          #+#    #+#             */
-/*   Updated: 2024/07/17 21:43:48 by kawaharadar      ###   ########.fr       */
+/*   Updated: 2024/07/18 00:08:39 by hakobori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,33 @@
 
 int	built_in(t_cmd *lst, t_status *status)
 {
-	if (ft_strncmp(lst -> cmd, "/bin/echo\0", 10) == 0)
+	if (ft_strncmp(lst->cmd, "/bin/echo\0", 10) == 0)
 		return (echo_func(lst));
-	else if (ft_strncmp(lst -> cmd, "echo\0", 5) == 0)
+	else if (ft_strncmp(lst->cmd, "echo\0", 5) == 0)
 		return (echo_func(lst));
-	if (ft_strncmp(lst -> cmd, "/bin/pwd\0", 9) == 0)
+	if (ft_strncmp(lst->cmd, "/bin/pwd\0", 9) == 0)
 		return (pwd_func(status->envm));
-	else if (ft_strncmp(lst -> cmd, "pwd\0", 4) == 0)
+	else if (ft_strncmp(lst->cmd, "pwd\0", 4) == 0)
 		return (pwd_func(status->envm));
-	if (ft_strncmp(lst -> cmd, "export\0", 7) == 0)
-		return (export_func2(lst -> arg, status, lst));
+	if (ft_strncmp(lst->cmd, "export\0", 7) == 0)
+		return (export_func2(lst->arg, status, lst));
+	if (ft_strncmp(lst->cmd, "exit\0", 5) == 0)
+		return (exit_func(lst));
 	return (0);
 }
 
 int	builtin2(t_cmd *lst, t_status *status)
 {
-	if (ft_strncmp(lst -> cmd, "export\0", 7) == 0)
-		return (export_func(lst -> arg, status, lst));
+	if (ft_strncmp(lst->cmd, "export\0", 7) == 0)
+		return (export_func(lst->arg, status, lst));
+	if (ft_strncmp(lst->cmd, "exit\0", 5) == 0)
+		return (exit_func(lst));
 	return (0);
 }
 
 int	check_builtin(char *cmd)
 {
-		if (ft_strncmp(cmd, "/bin/echo\0", 10) == 0)
+	if (ft_strncmp(cmd, "/bin/echo\0", 10) == 0)
 		return (1);
 	else if (ft_strncmp(cmd, "echo\0", 5) == 0)
 		return (1);
@@ -45,6 +49,8 @@ int	check_builtin(char *cmd)
 	else if (ft_strncmp(cmd, "pwd\0", 4) == 0)
 		return (1);
 	if (ft_strncmp(cmd, "export\0", 7) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "exit\0", 5) == 0)
 		return (1);
 	return (0);
 }
