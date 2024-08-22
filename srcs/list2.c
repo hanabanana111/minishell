19abd2e4efa2226b866c30bec3f4f9baa7bb0685
Ryo Kawaharada <rkawahar@c3r2s7.42tokyo.jp>
2024-08-22 19:13:52 +0900

    finish echo  -n

--- srcs/list2.c
+++ srcs/list2.c
@@ -6,7 +6,7 @@
 /*   By: rkawahar <rkawahar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 18:29:44 by rkawahar          #+#    #+#             */
-/*   Updated: 2024/08/22 18:55:46 by rkawahar         ###   ########.fr       */
+/*   Updated: 2024/08/22 19:12:44 by rkawahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int	search_arg(char *str1, t_info *lst)
 {
 	while (lst)
 	{
-		if (ft_strncmp(str1, lst -> str, sizeof(str1) + 1) == 0)
+		if (lst -> str && ft_strncmp(str1, lst -> str, sizeof(str1)) == 0)
 			break;
 		lst = lst -> next;
 	}
 	free(lst -> str);
-	lst -> str = NULL;
+	lst -> str = strdup("NULL");
 	if (lst -> key)
 		return (1);
 	else
@@ -34,17 +34,18 @@ t_cmd	*insert_flg(t_info *lst, t_cmd *ans)
 	int		i;
 
 	cmd_lst = ans;
+	i = 0;
 	while (cmd_lst)
 	{
 		while (cmd_lst -> arg[i])
 			i++;
-		cmd_lst -> flg = (int *)malloc(sizeof(int) * i);
+		cmd_lst -> flg = (int *)ft_calloc(sizeof(int), i);
 		if (cmd_lst -> flg == NULL)
 			error_exit("insert_flg");
 		i = 0;
 		while (cmd_lst -> arg[i])
 		{
-			cmd_lst -> flg[i] = serach_arg(cmd_lst -> arg[i], lst);
+			cmd_lst -> flg[i] = search_arg(cmd_lst -> arg[i], lst);
 			i++;
 		}
 		cmd_lst = cmd_lst -> next;
